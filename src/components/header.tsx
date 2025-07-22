e2ba482619078eee7824b76ff26230eb21ea7b1c
David-Jesse <djesse351@gmail.com>
2025-07-22 13:59:21 +0100

    added the navbar, footer, header and linker component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { createClient } from "@/prismicio";
+import Navbar from "@/components/navbar";
 
-type Props = {}
+const Header = async () => {
+  const client = createClient();
+  const settings = await client.getSingle("settings");
 
-const Header = (props: Props) => {
   return (
-    <div>header</div>
-  )
-}
+    <header className="fixed left-0 right-0 top-8 md:top-10 mx-auto flex z-50 w-full items-center justify-center">
+      <Navbar settings={settings} />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
